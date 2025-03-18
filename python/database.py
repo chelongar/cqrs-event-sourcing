405627dff058c0386be81cff29b27e93d2dba2cc
Masoud Chelongar <masoud.chelongar@yahoo.com>
2025-03-18 13:54:39 +0100

    PyCQRS: Variable name changed.
    Signed-off-by: Masoud Chelongar <masoud.chelongar@yahoo.com>

--- python/database.py
+++ python/database.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
+    dbase = SessionLocal()
     try:
-        yield db
+        yield dbase
     finally:
-        db.close()
+        dbase.close()
